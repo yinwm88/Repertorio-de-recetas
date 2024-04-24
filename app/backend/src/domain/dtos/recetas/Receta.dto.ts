ecbe607f574e03e28dbc23d77c819217735e1dac
Jose Luis <joseluis27@ciencias.unam.mx>
2024-04-23 21:21:12 -0600

    Correcion Receta.dto

--- app/backend/src/domain/dtos/recetas/Receta.dto.ts
+++ app/backend/src/domain/dtos/recetas/Receta.dto.ts
@@ -1,23 +1,15 @@
 export class RecetaDto{
 
     private constructor(
-        public readonly idReceta: number,
-        public readonly nombre: string,
-        public readonly tiempo: number,
-        public readonly proceso: string,
+        public readonly idReceta: number
     ){}
 
     static crearInstancia( objeto: { [key: string]: any }): [string?, RecetaDto?] {
-        const { idReceta, nombre, tiempo, proceso } = objeto;
+        const { idReceta } = objeto;
 
-        if (!idReceta) return ['Falta el id de la receta'];   
-        if (!nombre) return ['Falta el nombre de la receta'];   
-        if (!tiempo) return ['Falta el tiempo de la receta'];     
-        if (!proceso) return ['Falta el proceso de la receta'];     
+        if (!idReceta) return ['Falta el id de la receta'];     
 
-        if ( +tiempo < 0 ) return ['El timpo debe de ser positivo'];     
-
-        return [, new RecetaDto( idReceta, nombre, tiempo, proceso )];
+        return [, new RecetaDto( idReceta )];
     }
 
 }
