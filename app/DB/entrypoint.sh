a635618b73dffe7dd46b3bef49808cf99a4965c1
Asim-v <emoxic.sama@gmail.com>
2024-04-29 17:03:20 -0600

    Revert "Pobla la base de datos al correr contenedores"
    This reverts commit 11d5f6075e82d4fb8b2b580c99275430a3987a0c.

--- app/DB/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-psql -U postgres -d RECETAS -a -f /docker-entrypoint-initdb.d/DDL.sql
-psql -U postgres -d RECETAS -a -f /docker-entrypoint-initdb.d/DML.sql
-
-# Start the PostgreSQL service
-/usr/local/bin/docker-entrypoint.sh postgres
