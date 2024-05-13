1e16fbff9f81faeb23bd93c15e8552977cf1f9a0
wong <yinlen@ciencias.unam.mx>
2024-05-13 08:50:21 -0600

    se arreglo contenido de  TFuncionalidades

--- app/frontend/src/Componentes/LP/TFuncionalidad.js
+++ app/frontend/src/Componentes/LP/TFuncionalidad.js
@@ -11,8 +11,8 @@ const Accordion = styled((props) => (
 ))(({ theme, selected }) => ({
   border: `1px solid ${theme.palette.divider}`,
   backgroundColor: selected
-    ? 'rgba(255, 90, 95, 0.6)'
-      : 'rgba(255, 90, 95, 0.6)',
+    ? 'rgba(49,96,128, 0.8)'
+      : 'rgba(49,96,128, 0.8)',
     
   '&:not(:last-child)': {
     borderBottom: 0,
@@ -30,8 +30,8 @@ const AccordionSummary = styled((props) => (
 ))(({ theme }) => ({
   backgroundColor:
     theme.palette.mode === 'dark'
-    ? 'rgba(255, 90, 95, 0.8)' 
-    :  'rgba(255, 90, 95, 0.8)' ,
+    ? 'rgba(27,79,114, 0.8)' 
+    :  'rgba(27,79,114, 0.8)' ,
   flexDirection: 'row-reverse',
   '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
     transform: 'rotate(90deg)',
@@ -57,60 +57,60 @@ function TFuncionalidad() {
     <div>
       <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
         <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-          <Typography sx={{ fontFamily: 'Poppins' }} variant="h6">RECETAS IDEALES</Typography>
+          <Typography sx={{color: 'white', fontFamily: 'Poppins' }} variant="h6">RECETAS IDEALES</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography sx={{fontSize:'18px', fontFamily: 'Poppins' }}>
+          <Typography sx={{color: 'white',fontSize:'18px', fontFamily: 'Poppins' }}>
           Encuentra recetas adaptadas a tus preferencias y los ingredientes que tienes en tu cocina.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
         <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
-          <Typography sx={{ fontFamily: 'Poppins' }} variant="h6">FILTRAR RECETAS</Typography>
+          <Typography sx={{color: 'white', fontFamily: 'Poppins' }} variant="h6">FILTRAR RECETAS</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography sx={{fontSize:'18px', fontFamily: 'Poppins' }}>
+          <Typography sx={{color: 'white',fontSize:'18px', fontFamily: 'Poppins' }}>
           Encuentra fácilmente recetas por nombre, ingredientes o tipo de cocina.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
         <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
-          <Typography sx={{ fontFamily: 'Poppins' }} variant="h6">BUSCAR RECETAS</Typography>
+          <Typography sx={{color: 'white', fontFamily: 'Poppins' }} variant="h6">BUSCAR RECETAS</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography sx={{fontSize:'18px', fontFamily: 'Poppins' }}>
+          <Typography sx={{color: 'white',fontSize:'18px', fontFamily: 'Poppins' }}>
           Explora el repertorio de recetas generado para ti.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
         <AccordionSummary aria-controls="panel4d-content" id="panel4d-header">
-          <Typography sx={{ fontFamily: 'Poppins' }} variant="h6">MARCAR RECETAS FAVORITAS</Typography>
+          <Typography sx={{color: 'white', fontFamily: 'Poppins' }} variant="h6">MARCAR RECETAS FAVORITAS</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography sx={{fontSize:'18px', fontFamily: 'Poppins' }}>
+          <Typography sx={{color: 'white',fontSize:'18px', fontFamily: 'Poppins' }}>
             Guarda tus recetas favoritas para acceder rápidamente a ellas en el futuro.         
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion expanded={expanded === 'panel5'} onChange={handleChange('panel5')}>
         <AccordionSummary aria-controls="panel5d-content" id="panel5d-header">
-          <Typography sx={{ fontFamily: 'Poppins' }} variant="h6">AÑADIR RECETAS</Typography>
+          <Typography sx={{color: 'white', fontFamily: 'Poppins' }} variant="h6">AÑADIR RECETAS</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography sx={{fontSize:'18px', fontFamily: 'Poppins' }}>
+          <Typography sx={{color: 'white',fontSize:'18px', fontFamily: 'Poppins' }}>
             Crea y guarda tus propias recetas.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion expanded={expanded === 'panel6'} onChange={handleChange('panel6')}>
         <AccordionSummary aria-controls="panel6d-content" id="panel6d-header">
-          <Typography sx={{ fontFamily: 'Poppins' }} variant="h6">GENERAR LISTA DE INGREDIENTES</Typography>
+          <Typography sx={{color: 'white', fontFamily: 'Poppins' }} variant="h6">GENERAR LISTA DE INGREDIENTES</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography sx={{fontSize:'18px', fontFamily: 'Poppins' }} >
+          <Typography sx={{color: 'white',fontSize:'18px', fontFamily: 'Poppins' }} >
             Genera automáticamente una lista de compras basada en la receta que has seleccionado.          
           </Typography>
         </AccordionDetails>
