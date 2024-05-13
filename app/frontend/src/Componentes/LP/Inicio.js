1f42ac623da3aa44f34cdeb7c81589581e20b92a
wong <yinlen@ciencias.unam.mx>
2024-05-13 08:37:25 -0600

    se arreglo contenido de  TFuncionalidades

--- app/frontend/src/Componentes/LP/Inicio.js
+++ app/frontend/src/Componentes/LP/Inicio.js
@@ -13,15 +13,15 @@ function Inicio() {
         <Typography sx={{ fontFamily: 'Poppins', fontSize:'30px' }} variant="h5" component="h2" gutterBottom>
           ¿De qué se trata?
         </Typography>
-        <Typography sx={{ fontFamily: 'Poppins' }} variant="body1">
+        <p sx={{ fontFamily: 'Poppins' }} variant="body1">
           Kitchenify es tu asistente de cocina personalizado. Ofrecemos un repertorio de recetas adaptadas a tus ingredientes disponibles en la cocina, tus gustos, alergias y estado de salud.
-        </Typography>
-        <Typography sx={{ fontFamily: 'Poppins' }} variant="body1">
+        </p>
+        <p sx={{ fontFamily: 'Poppins' }} variant="body1">
           Nuestro objetivo es resolver problemas comunes en la cocina, como el desperdicio de ingredientes, la falta de control en la dieta y la indecisión sobre qué cocinar.
-        </Typography>
-        <Typography sx={{ fontFamily: 'Poppins' }} variant="body1">
+        </p>
+        <p sx={{ fontFamily: 'Poppins' }} variant="body1">
           Con Kitchenify, descubrirás nuevas recetas, aprenderás técnicas de cocina y te convertirás en un chef experto desde la comodidad de tu hogar.
-        </Typography>
+        </p>
 
         <Box sx={{ mt: 4 }}>
           <Typography sx={{ fontFamily: 'Poppins' }} variant="h4" gutterBottom>
