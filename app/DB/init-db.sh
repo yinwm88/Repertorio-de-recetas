de072b22b870cf448d37ec5194a1881b22d5bb3b
wong <yinlen@ciencias.unam.mx>
2024-05-26 17:40:58 -0600

    docker actualizado 2

--- app/DB/init-db.sh
+++ app/DB/init-db.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
 
-# Ejecuta los archivos DDL.sql y DML.sql
+# Esperar a que PostgreSQL esté listo
+until pg_isready -h localhost -p 5432; do
+  echo "Waiting for PostgreSQL to be ready..."
+  sleep 2
+done
+
+# Ejecutar los archivos DDL.sql y DML.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     \i /docker-entrypoint-initdb.d/01_DDL.sql
     \i /docker-entrypoint-initdb.d/02_DML.sql
