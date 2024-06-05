2156443a598675220b8bde7134ef3d8e8f26ab14
wong <yinlen@ciencias.unam.mx>
2024-06-05 15:07:41 -0600

    primer revert no es

--- app/frontend/src/Componentes/ShowLista.js
+++ app/frontend/src/Componentes/ShowLista.js
@@ -6,51 +6,28 @@ import ListItemText from '@mui/material/ListItemText';
 import IconButton from '@mui/material/IconButton';
 import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
 import { useAuth } from '../AuthContext';
-import Swal from 'sweetalert2';
 
 const ShowLista = () => {
     const { currentUser, getToken } = useAuth();
-    const [lista, setLista] = useState([]);
-    const [listaDatosMostrar, setListaDatosMostrar] = useState([]);
+    const [lista, setLista] = useState(null);
     const [error, setError] = useState(null);
     const [showLista, setShowLista] = useState(false);
     const [selectedItemId, setSelectedItemId] = useState(false); 
 
 
-    const getDatosIngrediente = async (id) => {
+    const getListaGenerada = async () => {
         try {
-            const token = await getToken();
-            const response = await fetch('http://localhost:3001/ingrediente/datos', {
+            const token = await getToken(); 
+            const response = await fetch('http://localhost:3001/ingrediente/listaCompras', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${token}` 
                 },
-                body: JSON.stringify({ id }),
-            });
 
-            if (!response.ok) {
-                throw new Error('No se pudo obtener los datos del ingrediente');
-            }
-
-            const data = await response.json();
-            return { nombre: data.nombre, unidad: data.unidad };
-        } catch (error) {
-            console.error(error);
-            return { nombre: 'Nombre desconocido', unidad: '' };
-        }
-    };
-
-    const getListaGenerada = async () => {
-        try {
-            const token = await getToken();
-            const response = await fetch('http://localhost:3001/ingrediente/listaCompras', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                },
-                body: JSON.stringify({ correo: currentUser }),
+                body: JSON.stringify({
+                    correo: currentUser, 
+                }),
             });
     
             if (!response.ok) {
@@ -58,117 +35,72 @@ const ShowLista = () => {
             }
     
             const data = await response.json();
-            setLista(data);
-            if(data.length !== 0){
-                setShowLista(); 
-                setSelectedItemId(true);   
-                listaTolistaDatos();    
-            }
+
+
+            // Obtener los nombres y unidades de los ingredientes
+            const listaConNombres = await Promise.all(data.map(async (item) => {
+                const { nombre, unidad } = await getDatosIngrediente(item.idingrediente);
+                return { ...item, nombre, unidad };
+            }));
+            
+            setLista(listaConNombres);
+            setShowLista(listaConNombres.length !== 0);
         } catch (error) {
             setError('No se pudo obtener la lista');
             console.error(error);
         }
         
     };
-    
-    const listaTolistaDatos = async () => {
-        console.log('Como esta  lista antes de  sacar listaDatos:', lista);
 
-            try {
-                const listaDatos = await Promise.all(lista.map(async (item) => {
-                    const datosIngrediente = await getDatosIngrediente(item.idingrediente);
-                    return {
-                        idIngrediente: item.idingrediente,
-                        nombre: datosIngrediente.nombre,
-                        cantidad: item.cantidad,
-                        unidad: datosIngrediente.unidad
-                    };
-                }));
-                setListaDatosMostrar(listaDatos);
-            } catch (error) {
-                console.error('Error al convertir lista a listaDatos:', error);
-            }
-    
-    };
-    
-    
 
-    //tratamos de obteener una listaGenerada, una vez obtenida entonces 
     useEffect(() => {
-            console.log('Como esta  lista despues de eliminars:', lista);
-            console.log('Como esta ListaDatosMostrar  despues de eliminars:', listaDatosMostrar);
-            getListaGenerada();
-    }, [!showLista]);
+        getListaGenerada();
+    }, [currentUser, getToken, showLista]); // Added showLista as a dependency
 
-
-    const handleComprarIngrediente = async (id) => {
-        console.log('Como esta ListaDatosMostrar  antes de eliminar:', listaDatosMostrar);
-        console.log('ID del ingrediente a comprar:', id);
+    const getDatosIngrediente = async (idingrediente) => {
         try {
-            const token = await getToken();
-            const payload = { id, correo: currentUser };
-            console.log('Payload:', JSON.stringify(payload));
-
-            const response = await fetch('http://localhost:3001/ingrediente/comprar/ingrediente', {
-                method: 'DELETE',
+            const token = await getToken(); 
+            const response = await fetch('http://localhost:3001/ingrediente/datos', {
+                method: 'POST',
                 headers: {
                     'Authorization': `Bearer ${token}`,
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(payload),
+
+                body: JSON.stringify({
+                    id: idingrediente
+                }),
             });
 
-            if (response.ok) {
-                Swal.fire({
-                    text: 'La lista de compra se vacio exitosamente!',
-                    icon: 'success',
-                });
-
-                // Actualizar listaDatosMostrar eliminando el elemento comprado
-                setListaDatosMostrar(prevListaDatosMostrar =>
-                    prevListaDatosMostrar.filter(item => item.idIngrediente !== id)
-                );
-
-            } else {
-                const errorText = await response.json();
-                console.error('Error response:', errorText);
-                Swal.fire({
-                    text: errorText.error || 'No se pudo marcar como comprado el ingrediente',
-                    icon: 'error',
-                });
+            if (!response.ok) {
+                throw new Error('No se pudo obtener los datos del ingrediente');
             }
+
+            const data = await response.json();
+            return { nombre: data.nombre, unidad: data.unidad }; // Suponiendo que el objeto devuelto tiene propiedades `nombre` y `unidad`
         } catch (error) {
             console.error(error);
-            Swal.fire({
-                text: 'Ocurrió un error al intentar comprar el ingrediente',
-                icon: 'error',
-            });
+
+            return { nombre: 'Nombre desconocido', unidad: '' }; // Valores por defecto en caso de error
         }
     };
-
+    
     return (
         <>
-            <div
-                style={{ display: 'flex', alignItems: 'center' }}>
-                    <h2 style={{ flex: 1 }}>Lista de Compras</h2>
-                    {selectedItemId && (
-                        <IconButton 
-                            aria-label="comprar" 
-                            sx={{ color: '#06A0D6' }}
-                            onClick={() => handleComprarIngrediente(lista[0].idingrediente)} 
-                            >
-                            <LocalGroceryStoreIcon />
-                        </IconButton>
-                    )}
-            </div>
-            {!showLista ? (
-                listaDatosMostrar.length > 0 ? (
+
+            <h2>Lista de Compras</h2>
+            {showLista ? (
+                lista ? (
                     <List>
-                        {listaDatosMostrar.map((item, index) => (
+                        {lista.map((item, index) => (
                             <ListItem key={index}>
                                 <ListItemText 
                                     primary={`${item.nombre}: ${item.cantidad} ${item.unidad}`} 
                                 />
+
+                                <IconButton edge="end" aria-label="comprar" sx={{ color: '#06A0D6' }}>
+                                    <LocalGroceryStoreIcon />
+                                </IconButton>
                             </ListItem>
                         ))}
                     </List>
